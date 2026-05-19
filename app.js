093c8fc5370fed77247069bfb58524c1acfbd71f
jlin92 <138638585+jlin92@users.noreply.github.com>
2026-05-19 18:08:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -630,7 +630,7 @@ function selectSeat(el, seatId, type){
   document.querySelectorAll('.seat.selected').forEach(function(s) {
     s.classList.remove('selected');
     s.classList.add(s.dataset.orig || 'available');
-  }
+  });
 
 
 // ----------------------------
